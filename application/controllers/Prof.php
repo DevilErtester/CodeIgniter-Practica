3e476adfb4981bca1c3f472be4eaba59ba1b78ed
Mario Chaves <xX1mario1Xx63@gmail.com>
2021-02-09 21:27:37 +0100

    commented the code

--- application/controllers/Prof.php
+++ application/controllers/Prof.php
@@ -60,11 +60,12 @@ class Prof extends CI_Controller
             $this->load->model('Alum_model');
             $this->load->model('Tutors_model');
 
-
+            // Getting the filter inputs
             $filter = $this->input->post('filter');
             $field  = $this->input->post('field');
             $search = $this->input->post('search');
 
+            // filtering the table content
             if (!empty($search) && !empty($field))
                 $alumnes = $this->Alum_model->getAlumnesWhereLike($field, $search);
             else if (isset($filter) && empty($search))
@@ -72,12 +73,15 @@ class Prof extends CI_Controller
             else
                 $alumnes = $this->Alum_model->getAllAlumnes();
 
+            // perparing data to send with the view load
             $data['cursos'] = $this->Tutors_model->getAllCursos();
             $data['taula'] = $alumnes;
             $data['form'] = $this->formAlu();
             $data['func'] = "index.php/Prof/Empresas";
             $data['funcName'] = "Empresas";
 
+
+            // Form submit for adding a new alumne
             if (isset($_POST['btnSubmit'])) {
                 if ($this->form_validation->run()) {
                     $alum = array(
@@ -90,6 +94,7 @@ class Prof extends CI_Controller
                     redirect('Prof/printAlumnes');
                 }
             }
+            // Dropdown button for adding cursos
             if (isset($_POST['newCurs'])) {
                 $arrayRes = explode(" ", $this->input->post('curs'));
                 $idAlu = $arrayRes[0];
@@ -99,6 +104,68 @@ class Prof extends CI_Controller
             }
             $this->load->view('prof_dashboard', $data);
         }
+    }
+    private function formAlu()
+    {
+        $this->load->library('form_validation');
+
+        $formAlu = form_open('Prof/printAlumnes');
+        $formAlu .= form_label('Email', 'mail');
+        $formAlu .= form_input(['name' => 'mail']);
+        $formAlu .= form_label('Nom', 'name');
+        $formAlu .= form_input(['name' => 'nom']);
+        $formAlu .= form_label('Telefon', 'telf');
+        $formAlu .= form_input(['name' => 'telf']);
+        $formAlu .= form_label('Curs FCT', 'cic_impar');
+        $formAlu .= form_input(['name' => 'fct']);
+        $formAlu .= form_submit('btnSubmit', 'Crear alumne');
+        $formAlu .= form_close();
+
+        $this->form_validation->set_rules('mail', 'Email', 'required|trim|xss_clean|is_unique[users.mail]');
+        $this->form_validation->set_rules('nom', 'Nom', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('telf', 'Telefon', 'required|trim|xss_clean|is_unique[alumnes.telefon]');
+
+        return $formAlu;
+    }
+    public function newAlu($alum)
+    {
+        if (!$this->is_logged()) {
+            $this->invalid();
+        } else {
+            $this->load->model('Alum_model');
+            $this->load->model('User_model');
+
+            $user = array(
+                'mail' => $alum['mail'],
+                'pass' => $this->random_password(),
+                'nom' => $alum['nom'],
+                'rol' => '1'
+            );
+
+            $this->User_model->newUser($user);
+            $idalum = $this->User_model->getIdUser($alum['mail']);
+
+            $newAlu = array(
+                'idAlumne' => $idalum,
+                'curs_FCT' => $alum['curs_FCT'],
+                'telefon' => $alum['telf']
+            );
+
+            $this->Alum_model->newAlum($newAlu);
+        }
+    }
+
+    public function delAlu($idAlu)
+    {
+        $this->load->model('Alum_model');
+        $this->Alum_model->delAlu($idAlu);
+        redirect('/Prof/printAlumnes');
+    }
+
+    public function editAlu($idAlu)
+    {
+
+        redirect('/Prof/printAlumnes');
     }
     // ===========================================================================================================
     // ======================GRUP ALUMNES=========================================================================
@@ -156,74 +223,6 @@ class Prof extends CI_Controller
         fclose($fp) or die("can't close file");
         return $alumnos;
     }
-
-    private function formAlu()
-    {
-        $this->load->library('form_validation');
-
-        $formAlu = form_open('Prof/printAlumnes');
-        $formAlu .= form_label('Email', 'mail');
-        $formAlu .= form_input(['name' => 'mail']);
-        $formAlu .= form_label('Nom', 'name');
-        $formAlu .= form_input(['name' => 'nom']);
-        $formAlu .= form_label('Telefon', 'telf');
-        $formAlu .= form_input(['name' => 'telf']);
-        $formAlu .= form_label('Curs FCT', 'cic_impar');
-        $formAlu .= form_input(['name' => 'fct']);
-        $formAlu .= form_submit('btnSubmit', 'Crear alumne');
-        $formAlu .= form_close();
-
-        $this->form_validation->set_rules('mail', 'Email', 'required|trim|xss_clean|is_unique[users.mail]');
-        $this->form_validation->set_rules('nom', 'Nom', 'required|trim|xss_clean');
-        $this->form_validation->set_rules('telf', 'Telefon', 'required|trim|xss_clean|is_unique[alumnes.telefon]');
-
-
-        return $formAlu;
-    }
-
-
-
-    public function newAlu($alum)
-    {
-        if (!$this->is_logged()) {
-            $this->invalid();
-        } else {
-            $this->load->model('Alum_model');
-            $this->load->model('User_model');
-
-            $user = array(
-                'mail' => $alum['mail'],
-                'pass' => $this->random_password(),
-                'nom' => $alum['nom'],
-                'rol' => '1'
-            );
-
-            $this->User_model->newUser($user);
-            $idalum = $this->User_model->getIdUser($alum['mail']);
-
-            $newAlu = array(
-                'idAlumne' => $idalum,
-                'curs_FCT' => $alum['curs_FCT'],
-                'telefon' => $alum['telf']
-            );
-
-            $this->Alum_model->newAlum($newAlu);
-        }
-    }
-
-    public function delAlu($idAlu)
-    {
-        $this->load->model('Alum_model');
-        $this->Alum_model->delAlu($idAlu);
-        redirect('/Prof/printAlumnes');
-    }
-
-    public function editAlu($idAlu)
-    {
-
-        redirect('/Prof/printAlumnes');
-    }
-
 
     // ===========================================================================================================
     // ======================EMPRESAS=============================================================================
